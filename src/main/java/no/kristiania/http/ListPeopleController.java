786d94179d03e57ac3c3706993085f51779d86de
cathrinejakobsen <72168843+cathrinejakobsen@users.noreply.github.com>
2021-11-01 11:33:24 +0100

    Base project reference

---
+++ src/main/java/no/kristiania/http/ListPeopleController.java
@@ -0,0 +1,24 @@
+package no.kristiania.http;
+
+import no.kristiania.person.Person;
+import no.kristiania.person.PersonDao;
+
+import java.sql.SQLException;
+
+public class ListPeopleController implements no.kristiania.http.HttpController {
+    private PersonDao personDao;
+
+    public ListPeopleController(PersonDao personDao) {
+        this.personDao = personDao;
+    }
+
+    @Override
+    public HttpMessage handle(HttpMessage request) throws SQLException {
+        String response = "";
+
+        for (Person person : personDao.listAll()){
+            response += "<div>" + person.getLastName() + ", " + person.getFirstName() + "</div>";
+        }
+        return new HttpMessage("HTTP/1.1 200 OK", response);
+    }
+}
