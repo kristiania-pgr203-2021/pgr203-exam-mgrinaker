16e545af3eb86c1e6705c3674e0eb0b538ef4ed4
cathrinejakobsen <cjkbsn@gmail.com>
2021-11-09 14:24:46 +0100

    Added 303 See other

--- src/test/java/no/kristiania/person/PersonDaoTest.java
+++ src/test/java/no/kristiania/person/PersonDaoTest.java
@@ -38,7 +38,7 @@ public class PersonDaoTest {
 
     public static Person examplePerson() {
         Person person = new Person();
-        person.setFirstName(TestData.pickOne("Johannes", "Jill", "Jane", "James", "Jacob", "Nora", "Emil", "Noah", "Emma", "Maja", "Oliver", "Filip", "Lukas","Liam", "Henrik", "Sofia", "Emilie"));
+        person.setFirstName(TestData.pickOne("Johanne", "Jill", "Jane", "James", "Jacob", "Nora", "Emil", "Noah", "Emma", "Maja", "Oliver", "Filip", "Lukas","Liam", "Henrik", "Sofia", "Emilie"));
         person.setLastName(TestData.pickOne("Hansen", "Johansen", "Olsen", "Larsen", "Andersen", "Pedersen", "Nilsen", "Kristiansen", "Jensen", "Karlsen", "Johnsen"));
         person.setMailAddress(TestData.pickOne("romantic01@online.no", "crypt@gmail.com", "kramulous@comcast.net", "gomor@icloud.com", "dbrobins@att.net", "lampcht@online.net", "mleary@mac.com", "gward@verizon.net", "dexter@msn.com", "oiyou-47@mail.com", "essi389@mail.com", "brovade5@ymail.com", "tebei41@yopl.com", "pauda9@mail.com", "blomster@yahoo.com", "boot.32@gmail.com"));
         person.setProfessionId(TestData.pickOneInteger(1, 2, 2));
